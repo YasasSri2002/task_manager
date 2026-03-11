a9eb5bca252481b849e8a90c513835cb20aa3942
YasasSri2002 <srimedagedara234@gmail.com>
2026-03-11 20:20:59 +0530

    front end/ admin dashboard

---
+++ task_manager_frontend/app/admin/[id]/dashboard/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useParams } from "next/navigation";
+import LoadingScreen from "@/components/loadingScreen";
+import { Navbar } from "@/app/navBar";
+import TasksPage from "@/app/task/TaskPage";
+import { useState, useEffect } from "react";
+import { UserResponseDto } from "@/dto/user";
+import Cookies from "js-cookie";
+import { getUserById } from "@/services/user/getByid";
+
+export default function AdminDashboardPage(){
+    const params = useParams();
+    const id = params.id as string;
+    const[userData,setUserData] =useState<UserResponseDto>();
+    const[role,setRole] = useState("ADMIN");
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const cookieRole = Cookies.get('x-user-role');
+            const data = await getUserById(id);
+            setUserData(data);
+            setRole(cookieRole!);
+        };
+        fetchUser();
+        
+        
+    }, [id]);
+
+    if (!userData) return <LoadingScreen message="Fetching user data..." />;
+
+    return (
+        <main>
+            <Navbar username={userData.username} role={role} />
+            {/* <TasksPage userId={id} tasks={taskList}/> */}
+        </main>
+    );
+}
